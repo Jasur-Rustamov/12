c08c3861ef5a1c702285306e150b2f348b9534c8
Jasur-Rustamov <isakovamatlyuba1959@gmail.com>
2024-11-19 00:00:20 +0500

    hometask

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html, css}"],
+  theme: {
+    extend: {
+      colors: {
+        bcolor: "#013CC6",
+        "light-gray": "#666666",
+        bbg: "#F8F8F8",
+        "bor-gray": "#CCCCCC",
+        jijjj: "#FFBC0F",
+      },
+      fontFamily: {
+        popo: "Poppins",
+      },
+    },
+  },
+  plugins: [],
+};
